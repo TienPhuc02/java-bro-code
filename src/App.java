3c9fbed93348740dba5b69cb54f1ec1d8a5a1950
TienPhuc02 <dtp2352002@gmail.com>
2024-02-11 20:27:21 +0700

    java array

--- src/App.java
+++ src/App.java
@@ -4,11 +4,24 @@ import javax.swing.*;
 import java.util.*;
 public class App {
     public static void main(String[] args)  {
-       //for loop
+       //nested loop 
 
-       for(int i=10;i>=0;i--){
-        System.out.println(i);
+
+       //array
+
+       String[] cars ={"Camaro","Tesla","Corvette","BWM"};
+       cars[0]="Mustang";
+       System.out.println(cars[3]);
+
+
+       //write array 
+       String[] cars1=new String[3];
+       cars1[0]="Camaro";
+       cars1[1]="Tesla";
+       cars1[2]="Corvette";
+       System.out.println(cars1[2]);
+       for(int i =0; i<cars1.length;i++){
+        System.out.println(cars[i]);
        }
-       System.out.println("Happy new year!");
     }
 }
