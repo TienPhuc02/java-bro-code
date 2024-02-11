20d080e4135b67373438d79fb0d83c920a2bdbe4
TienPhuc02 <dtp2352002@gmail.com>
2024-02-11 21:24:15 +0700

    java string method basic

--- src/App.java
+++ src/App.java
@@ -4,30 +4,17 @@ import javax.swing.*;
 import java.util.*;
 public class App {
     public static void main(String[] args)  {
-      //array 2d 
+    //string method : equals , equalsIgnoreCase,length,charAt,indexOf,isEmpty,toUpperCase,toLowerCase,trim,replace
 
-      String[][] cars={
-        {"Camero","Corvette","Silverado"},
-        {"Mustang","Ranger","F-150"},
-        {"Ferrari","Lambo","Tesla"},
-      };
+    String name="Phuc";
+    // boolean result = name.equalsIgnoreCase("Phuc");//so sanh chinh xac
+    // int result =name.length();
+    // char result =name.charAt(0);
+    // int result=name.indexOf("P");
+    
+    boolean result=name.isEmpty();
+    System.out.println(result);
 
-    //   String[][] cars =new String[3][3];
-    //     cars[0][0]="Camero";
-    //     cars[0][1]="Corvette";
-    //     cars[0][2]="Silverado";
-    //     cars[1][0]="Mustang";
-    //     cars[1][1]="Ranger";
-    //     cars[1][2]="F-150";
-    //     cars[2][0]="Ferrari";
-    //     cars[2][1]="Lambo";
-    //     cars[2][2]="Tesla";
-        for(int i =0;i<cars.length;i++){
-            System.out.println();
-            for(int j=0;j<cars[i].length;j++){
-                System.out.println(cars[i][j]);
-            }
-        }
 
     }
 }
