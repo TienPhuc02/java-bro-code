d50564f156aa3473579ddcea49e698f971f32870
TienPhuc02 <dtp2352002@gmail.com>
2024-02-11 16:53:27 +0700

    java math

--- src/App.java
+++ src/App.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 import javax.swing.*;
 public class App {
     public static void main(String[] args)  {
+    double x= 3.14;
+    double y = 10;
+    double z= Math.max(x, y);
+    double t= Math.min(x, y);
+    double q= Math.abs(y);//tim gia tri tuyet doi
+    double w= Math.sqrt(y);//tim gia tri tuyet doi
+    System.out.println(z);
+    System.out.println(t);
+    System.out.println(q);
 
-        String name = JOptionPane.showInputDialog("Enter your name");
-        JOptionPane.showMessageDialog(null, "Hello " +name );
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-        JOptionPane.showMessageDialog(null, "I am " +age + "years old." );
-        double heigt = Double.parseDouble(JOptionPane.showInputDialog("Enter your heigt"));
-        JOptionPane.showMessageDialog(null, "I am " +heigt + "cm" );
+    //Math : round , ceil,floor,
 
-
-
+    //lab
+    //a,b,c
+    Scanner scanner= new Scanner(System.in);
+    double canhVuong1= scanner.nextDouble();
+    double canhVuong2= scanner.nextDouble();
+    double canhHuyen= Math.sqrt(canhVuong1*canhVuong1 + canhVuong2*canhVuong2);
+    System.out.println(canhHuyen);
+    scanner.close();
     }
 }
