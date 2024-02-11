8751276948652e7c317a9925955829376fbf051c
TienPhuc02 <dtp2352002@gmail.com>
2024-02-11 16:28:34 +0700

    toan tu , ep kieu

--- src/App.java
+++ src/App.java
@@ -3,17 +3,12 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args)  {
 
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("what is your name ?" );
-      String name =scanner.nextLine();
-      System.out.println("how old are you?");
-      int age =scanner.nextInt();
-      scanner.nextLine();
-      System.out.println("What is favourite food ?");
-      String food =scanner.nextLine();
-      System.out.println("Hello " + name);
-      System.out.println("I am  " + age +" years old.");
-      System.out.println("My favorite food is : " +  food);
+     //expression: bieu thuc = operands : toan tu  +  operators : toan hang
+     //operands: types, variable,numbers, quantity
+     //operators: + - * / % ++ 
+     double friends =10;
+     friends =(double)friends/3 ;
+        
 
     }
 }
