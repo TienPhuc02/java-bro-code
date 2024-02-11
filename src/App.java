52523e1d242a45840c420e4f820ef7116a33b8ac
TienPhuc02 <dtp2352002@gmail.com>
2024-02-11 16:06:54 +0700

    variable java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+
+        int x; // khai báo
+        x =123; // gán giá trị
+        int a =235; //khởi tạo biến và giá trị
+        System.out.println(a);// number 
+        System.out.println("My number is : " + a );
+
+        long b =123123123123l; // khai báo một giá trị long 
+
+        float c= 3.14f;
+        double d=3.14;
+        boolean z=true;
+        char symbol = '@';
+        String name = " Phuc";
+         
+
     }
 }
