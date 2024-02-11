ee4b3c5f1e57c79930682c928449c9d6a56c46e5
TienPhuc02 <dtp2352002@gmail.com>
2024-02-11 19:56:47 +0700

    if else statement

--- src/App.java
+++ src/App.java
@@ -4,15 +4,17 @@ import javax.swing.*;
 import java.util.*;
 public class App {
     public static void main(String[] args)  {
-        Random random =new Random();
-        int x  = random.nextInt(10); //0->10
-        int y  = random.nextInt(10)+1; //1->10
-        double z= random.nextDouble();
-        boolean BOolean =random.nextBoolean();
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(z);
-        System.out.println(BOolean);
-    
+        //if statement
+
+        int age =75;
+        if(age==19){
+            System.out.println("You are  an adult!");
+        }
+        else if(age >=75 ){
+            System.out.println("oke Bro");
+        }
+        else{
+            System.out.println("You are not adult");   
+        }
     }
 }
