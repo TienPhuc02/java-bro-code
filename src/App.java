d49d8b20ee63d2c8baea735aac7fa61deebddd24
TienPhuc02 <dtp2352002@gmail.com>
2024-02-11 20:38:15 +0700

    java array 2d

--- src/App.java
+++ src/App.java
@@ -4,24 +4,30 @@ import javax.swing.*;
 import java.util.*;
 public class App {
     public static void main(String[] args)  {
-       //nested loop 
+      //array 2d 
 
+      String[][] cars={
+        {"Camero","Corvette","Silverado"},
+        {"Mustang","Ranger","F-150"},
+        {"Ferrari","Lambo","Tesla"},
+      };
 
-       //array
+    //   String[][] cars =new String[3][3];
+    //     cars[0][0]="Camero";
+    //     cars[0][1]="Corvette";
+    //     cars[0][2]="Silverado";
+    //     cars[1][0]="Mustang";
+    //     cars[1][1]="Ranger";
+    //     cars[1][2]="F-150";
+    //     cars[2][0]="Ferrari";
+    //     cars[2][1]="Lambo";
+    //     cars[2][2]="Tesla";
+        for(int i =0;i<cars.length;i++){
+            System.out.println();
+            for(int j=0;j<cars[i].length;j++){
+                System.out.println(cars[i][j]);
+            }
+        }
 
-       String[] cars ={"Camaro","Tesla","Corvette","BWM"};
-       cars[0]="Mustang";
-       System.out.println(cars[3]);
-
-
-       //write array 
-       String[] cars1=new String[3];
-       cars1[0]="Camaro";
-       cars1[1]="Tesla";
-       cars1[2]="Corvette";
-       System.out.println(cars1[2]);
-       for(int i =0; i<cars1.length;i++){
-        System.out.println(cars[i]);
-       }
     }
 }
