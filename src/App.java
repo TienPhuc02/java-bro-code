ce2020f4973f637779efda7e3ba91b011e8de09f
TienPhuc02 <dtp2352002@gmail.com>
2024-02-11 20:11:58 +0700

    while loop

--- src/App.java
+++ src/App.java
@@ -4,32 +4,23 @@ import javax.swing.*;
 import java.util.*;
 public class App {
     public static void main(String[] args)  {
-        String day ="Friday";
-        switch (day) {
-            case "Sunday":
-                System.out.println("It is Sunday");
-                break;
-            case "Monday":
-                System.out.println("It is Monday");
-                break;
-            case "Tuesday":
-                System.out.println("It is Tuesday");
-                break;
-            case "Wednesday":
-                System.out.println("It is Wednesday");
-                break;
-            case "Thursday":
-                System.out.println("It is Thursday");
-                break;
-            case "Friday":
-                System.out.println("It is Friday");
-                break;
-            case "Saturday":
-                System.out.println("It is Saturday");
-                break;
-            default : 
-            System.out.println("Khong co ket qua thi chay vao day");
-            break;
-        }
+       //logic operator:&& ,|| , !
+
+
+       // while loop 
+       Scanner scanner = new Scanner(System.in);
+       String name ="";
+       String name1="";
+       while(name.isBlank()){
+        System.out.println("Enter your name :");
+        name =scanner.nextLine();
+       }
+
+       //do while -> thuc hien truoc kiem tra sau
+       do {
+        System.out.println("Enter your name :");
+        name1 =scanner.nextLine();
+       }while(name1.isBlank());
+       System.out.println("hello " + name);
     }
 }
