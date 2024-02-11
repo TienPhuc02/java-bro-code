7dbc04dd630b2dbe0027a25681d18835b04dc90f
TienPhuc02 <dtp2352002@gmail.com>
2024-02-11 16:23:55 +0700

    scanner package

--- src/App.java
+++ src/App.java
@@ -1,15 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args)  {
 
-      //swap 2 variable
-      String x= "water";
-      String y = "Kool-Aid";
-      String temp =x;
-      x=y;
-      y=temp; 
-      System.out.println("x : " + x );
-      System.out.println("y : " + y );
-
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("what is your name ?" );
+      String name =scanner.nextLine();
+      System.out.println("how old are you?");
+      int age =scanner.nextInt();
+      scanner.nextLine();
+      System.out.println("What is favourite food ?");
+      String food =scanner.nextLine();
+      System.out.println("Hello " + name);
+      System.out.println("I am  " + age +" years old.");
+      System.out.println("My favorite food is : " +  food);
 
     }
 }
