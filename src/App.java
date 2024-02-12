953599dde764b488a94ac5223e4d42856d330f83
TienPhuc02 <dtp2352002@gmail.com>
2024-02-12 07:30:53 +0700

    for each

--- src/App.java
+++ src/App.java
@@ -2,25 +2,20 @@ import javax.swing.*;
 import java.util.*;
 public class App {
     public static void main(String[] args)  {
-   //array list 2d
-   ArrayList<ArrayList<String>> groceryList =new ArrayList<ArrayList<String>>();
-   ArrayList<String> bakeryList=new ArrayList<String>();
-   bakeryList.add("pasta");
-   bakeryList.add("parlic beard");
-   bakeryList.add("donuts");
-   ArrayList<String> productList=new ArrayList<String>();
-   productList.add("tomatoes");
-   productList.add("zucchini");
-   productList.add("peppers");
-   ArrayList<String> drinksList=new ArrayList<String>();
-   drinksList.add("soda");
-   drinksList.add("coffe");
-   drinksList.add("peppers");
-   System.out.println(drinksList.get(0));
-   groceryList.add(bakeryList);
-   groceryList.add(productList);
-   groceryList.add(drinksList);
-
-System.out.println(groceryList);
+   // for-each = 	traversing technique to iterate through the elements in an array/collection
+		//				less steps, more readable
+		//				less flexible
+		
+		//String[] animals = {"cat","dog","rat","bird"};
+		ArrayList<String> animals = new ArrayList<String>();
+		
+		animals.add("cat");
+		animals.add("dog");
+		animals.add("rat");
+		animals.add("bird");
+		
+		for(String i : animals) {
+			System.out.println(i);
+		}
     }
 }
