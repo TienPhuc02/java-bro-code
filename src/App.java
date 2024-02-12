37114da6c719b0436a54d2f318286506f982ad81
TienPhuc02 <dtp2352002@gmail.com>
2024-02-12 07:54:17 +0700

    final keyword

--- src/App.java
+++ src/App.java
@@ -2,38 +2,10 @@ import javax.swing.*;
 import java.util.*;
 public class App {
     public static void main(String[] args)  {
-  // printf() = 	an optional method to control, format, and display text to the console window
-		//				two arguments = format string + (object/variable/value)
-		//				% [flags] [precision] [width] [conversion-character]
+        final double PI = 3.14159;
 				
-		boolean myBoolean = true;
-		char myChar = '@';
-		String myString = "Bro";
-		int myInt = 50;
-		double myDouble = 1000;
-			
-		// [conversion-character]
-		//System.out.printf("%b",myBoolean);
-		//System.out.printf("%c",myChar);
-		//System.out.printf("%s",myString);
-		//System.out.printf("%d",myInt);
-		//System.out.printf("%f",myDouble);
+		//PI = 4;  //You can't change a final variable
 		
-		//[width]
-		// minimum number of characters to be written as output
-		//System.out.printf("Hello %10s",myString);
-		
-		//[precision]
-		// sets number of digits of precision when outputting floating-point values
-		//System.out.printf("You have this much money %.1f",myDouble);
-		
-		// [flags]
-		// adds an effect to output based on the flag added to format specifier
-		// - : left-justify
-		// + : output a plus ( + ) or minus ( - ) sign for a numeric value
-		// 0 : numeric values are zero-padded
-		// , : comma grouping separator if numbers > 1000
-		
-		//System.out.printf("You have this much money %,f",myDouble);
+		System.out.println(PI);
 }
 }
